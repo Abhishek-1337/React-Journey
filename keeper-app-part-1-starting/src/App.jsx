14f5073e4ab41a1a695b73e950f1e729a2335a65
Abhishek-1337 <abhishekrinku4@gmail.com>
2023-01-12 18:45:01 +0530

    initial commit keeper app

---
+++ keeper-app-part-1-starting/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Component/Header";
+import Footer from "./Component/Footer";
+import Note from './Component/Note';
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <Footer />
+            <Note />
+        </div>
+    );
+}
+
+export default App;
