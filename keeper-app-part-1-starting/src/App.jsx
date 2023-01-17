c955b0b64b7e62a45783806604c761772022cebe
Abhishek-1337 <abhishekrinku4@gmail.com>
2023-01-17 18:12:43 +0530

    map component

--- keeper-app-part-1-starting/src/App.jsx
+++ keeper-app-part-1-starting/src/App.jsx
@@ -2,13 +2,24 @@ import React from "react";
 import Header from "./Component/Header";
 import Footer from "./Component/Footer";
 import Note from './Component/Note';
+import notes from "./notes.js";
+
 
 function App() {
+    console.log(notes);
     return (
         <div>
             <Header />
             <Footer />
-            <Note />
+            {
+                notes.map(item => {
+                    return (<Note 
+                        key={item.id}
+                        title={item.title}
+                        content={item.content}
+                    />);    
+                })
+            }
         </div>
     );
 }
